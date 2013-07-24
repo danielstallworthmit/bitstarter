5c983432d7577bf344820b2cd8264ab67b4f1274
Daniel Stallworth <danieljs@mit.edu>
2013-07-24 14:17:06 -0400

    sync read2

--- web.js
+++ web.js
@@ -1,18 +1,12 @@
 var express = require('express');
-var fs = require('fs');
-
-var content = fs.readFileSync('/index.html', 'utf8').toString();
-console.log(content);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(content);
+  response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-	
